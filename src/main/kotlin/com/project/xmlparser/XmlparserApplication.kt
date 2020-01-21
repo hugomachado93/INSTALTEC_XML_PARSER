59c997c747a9e3139a094dc576f6d9f374527db6
Your Name <you@example.com>
2020-01-20 22:31:13 -0300

    Adiciona eureka client

--- src/main/kotlin/com/project/xmlparser/XmlparserApplication.kt
+++ src/main/kotlin/com/project/xmlparser/XmlparserApplication.kt
@@ -3,6 +3,7 @@ package com.project.xmlparser
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.xml.sax.helpers.DefaultHandler
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.io.File
@@ -10,6 +11,7 @@ import javax.xml.parsers.SAXParserFactory
 
 @EnableSwagger2
 @SpringBootApplication
+@EnableDiscoveryClient
 class XmlparserApplication : CommandLineRunner {
     override fun run(vararg args: String?) {
     }
