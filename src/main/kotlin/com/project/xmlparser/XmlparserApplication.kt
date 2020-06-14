d742e43e94d028768c7eab303c4c1916fcf04b92
Hugo Machado <hugo.machado@m4u.com.br>
2020-06-14 16:22:40 -0300

    Remove libs nao usadas

--- src/main/kotlin/com/project/xmlparser/XmlparserApplication.kt
+++ src/main/kotlin/com/project/xmlparser/XmlparserApplication.kt
@@ -1,21 +1,10 @@
 package com.project.xmlparser
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.xml.sax.helpers.DefaultHandler
-import springfox.documentation.swagger2.annotations.EnableSwagger2
-import java.io.File
-import javax.xml.parsers.SAXParserFactory
 
-@EnableSwagger2
-@SpringBootApplication
-@EnableDiscoveryClient
-class XmlparserApplication : CommandLineRunner {
-    override fun run(vararg args: String?) {
-    }
-}
+@SpringBootApplication(proxyBeanMethods = false)
+class XmlparserApplication
 
 fun main(args: Array<String>) {
     runApplication<XmlparserApplication>(*args)
